ada4286dcb337c5e7994ef038eb2c17a8f33069e
NicolasDouillet <nicolas.douillet@free.fr>
2024-12-27 11:48:34 +0100

    remove_outliers : + complete header

--- src/point_set_processing/remove_outliers.m
+++ src/point_set_processing/remove_outliers.m
@@ -1,5 +1,5 @@
 function [P_out, C_out, N_out] = remove_outliers(P_in, k, pct2rm, C_in, N_in)
-% remove_outliers : function to remove rm_pct percentage of the outliers
+%% remove_outliers : function to remove rm_pct percentage of the outliers
 % present in the given 3D point set P_in.
 %
 %%% Author : nicolas.douillet (at) free.fr, 2024.
@@ -7,28 +7,41 @@ function [P_out, C_out, N_out] = remove_outliers(P_in, k, pct2rm, C_in, N_in)
 %
 %%% Input arguments
 %
-% - P_in
+%          [ |    |    |  ]
+% - P_in = [X_in Y_in Z_in], real matrix double, the input point set, size(P_in) = [nb_input_points,3]. Mandatory argument.
+%          [ |    |    |  ]
 %
-% - k :      positive integer scalar double, the number of nearest neighbors. k >= size(P_in,1).
+% - k :      positive integer scalar double, the number of nearest neighbors. k <= size(P_in,1). In practice 4 <= k <= 50.
 %
 % - pct2rm : positive integer scalar double in the range |[0; 100]|, the percentage of points to remove.
 %
-% - C_in
+%          [ |    |    |  ]
+% - C_in = [R_in G_in B_in], integer matrix double, the input color set, size(C_in) = [nb_input_points,3]. Optional argument.
+%          [ |    |    |  ]
 %
-% - N_in
+%          [  |     |     |  ]
+% - N_in = [Nx_in Ny_in Nz_in], real matrix double, the input normals set, size(N_in) = [nb_input_points,3]. Optional argument.
+%          [  |     |     |  ]
 %
 %
 %%% Output arguements
 %
-% - P_out
+%           [  |     |     |  ]
+% - P_out = [X_out Y_out Z_out], real matrix double, the output point set, size(P_out) = [nb_output_points,3],
+%           [  |     |     |  ]
 %
-% - C_out
+%           where nb_output_points = nb_input_points - nb_duplicata. Mandatory argument.
 %
-% - N_out
+%           [ |     |     |   ]
+% - C_out = [R_out G_out B_out], integer matrix double, the output color set, size(C_out) = [nb_output_points,3]. Optional argument.
+%           [ |     |     |   ]
+%
+%           [  |      |      |   ]
+% - N_out = [Nx_out Ny_out Nz_out], real matrix double, the output normals set, size(N_in) = [nb_output_points,3]. Optional argument.
+%           [  |      |      |   ]
 
 
-
-% Body
+%% Body
 nb_pts = size(P_in,1);
 dst = zeros(nb_pts,k);
 
