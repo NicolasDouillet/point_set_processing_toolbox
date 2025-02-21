6c0848a8d722b0456f16a1ef969d95d889f97f20
NicolasDouillet <nicolas.douillet@free.fr>
2025-02-21 15:42:44 +0100

    improved Reuleaux tetrahedron

--- src/training_point_sets_generation/Reuleaux_tetrahedron.m
+++ src/training_point_sets_generation/Reuleaux_tetrahedron.m
@@ -40,101 +40,136 @@ P2 = [2*sqrt(2)/3 0 -1/3]';
 P3 = [-sqrt(2)/3 sqrt(6)/3 -1/3]';
 P4 = [-sqrt(2)/3 -sqrt(6)/3 -1/3]';
 
-C = 0.5*(P3 + P4);
-R = norm(P1 - C);
-u = P2;
 
-x_C = C(1,1);
-z_C = C(3,1);
+edge_length = norm(P1-P2); %  = 2*sqrt(6)/3
 
-alpha = acos(4/3/sqrt(2));
+P123 = sample_and_curve_triangle(P1,P2,P3,nb_samples,0.85,random_sampling);
+P123 = inflate_triangle_sample_from_opposite_vertex(P123,P4',edge_length);
 
+% Tetrahedron faces rotations
+Rmy = @(theta) [cos(theta) 0 -sin(theta);
+                0          1  0;
+                sin(theta) 0  cos(theta)];
+
+Rmz = @(theta) [cos(theta) -sin(theta) 0;
+                sin(theta)  cos(theta) 0;
+                0           0          1];
+                        
+P134 = (Rmz(2*pi/3)*P123')';
+P142 = (Rmz(2*pi/3)*P134')';
+P234 = (Rmy(-acos(-1/3))*Rmz(pi/3)*P142')';
+            
+P = [P123; P134; P142; P234];
+P = unique(P, 'rows');
+
+
+end % Reuleaux_tetrahedron
+
+
+%% sample_and_curve_triangle subfunction
+function I = sample_and_curve_triangle(P1, P2, P3, nb_samples, warp, random_sampling)
+%
+% Authors : nicolas.douillet (at) free.fr, 2017-2024.
+%           Gerd Wachsmuth,                     2021.
+
+
+% Create sampling grid
+Ndim = size(P1,1);
+
+% (P0P1, P0P2) base
+u = (P2 - P1);
+v = (P3 - P1);
+
+I = zeros((nb_samples+1)*(nb_samples+2)/2, Ndim);
+    
 if ~random_sampling
     
-    theta = linspace(pi/2-alpha,0,nb_samples+1);
+    k = 1;
     
-    x_SA = R*cos(theta) + x_C;
-    z_SA = (abs(R^2 - (x_SA - x_C).^2)).^0.5 + z_C;
-    y_SA = zeros(1,length(x_SA));
-    
-    SA = [x_SA;y_SA;z_SA];
-    
-    Rmz = [-0.5         -0.5*sqrt(3) 0;
-            0.5*sqrt(3) -0.5         0;
-            0            0           1];
-    
-    Rm = [u(1,1)^2-0.5*(1-u(1,1)^2) 1.5*u(1,1)*u(2,1)-u(3,1)*sqrt(3)/2 1.5*u(1,1)*u(3,1)+u(2,1)*sqrt(3)/2;
-        1.5*u(1,1)*u(2,1)+u(3,1)*sqrt(3)/2 u(2,1)^2-0.5*(1-u(2,1)^2) 1.5*u(2,1)*u(3,1)-u(1,1)*sqrt(3)/2;
-        1.5*u(1,1)*u(3,1)-u(2,1)*sqrt(3)/2 1.5*u(2,1)*u(3,1)+u(1,1)*sqrt(3)/2 u(3,1)^2-0.5*(1-u(3,1)^2)];
-    
-    SB = Rmz * SA;   
-    BA = Rm * Rm * SA;
-    
-    % Rectangular Coons patch
-    nu = length(theta);
-    nv = nu;
-    
-    AB = fliplr(BA);
-    
-    SAB = zeros(nu,nv,3);
-    SAB(:,1,:)   = cat(3,cat(3,SA(1,:)',SA(2,:)'),SA(3,:)');
-    SAB(end,:,:) = cat(3,cat(3,AB(1,:)',AB(2,:)'),AB(3,:)');
-    SAB(:,end,:) = cat(3,cat(3,SB(1,:)',SB(2,:)'),SB(3,:)');
-    SAB(1,:,:)   = repmat(cat(3,cat(3,0,0),1),[1 length(theta) 1]);
-    
-    
-    for n = 1:nu
+    % Sampling & vertices generation
+    for m = 0:nb_samples
         
-        for p = 1:nv
+        for n = 0:nb_samples
             
-            u = n/(nu-1);
-            v = p/(nv-1);
-            
-            SAB(n,p,1) = (1-u)*SAB(1,p,1)+...
-                         (1-v)*SAB(n,1,1)+...
-                             u*SAB(end,p,1) +...
-                             v*SAB(n,end,1) +...
-                   (u-1)*(1-v)*SAB(1,1,1)-...
-                           u*v*SAB(end,end,1)+...
-                       u*(v-1)*SAB(end,1,1)+...
-                       v*(u-1)*SAB(1,end,1);
-            
-            SAB(n,p,2) = (1-u)*SAB(1,p,2)+...
-                         (1-v)*SAB(n,1,2)+...
-                             u*SAB(end,p,2) +...
-                             v*SAB(n,end,2) +...
-                   (u-1)*(1-v)*SAB(1,1,2)-...
-                           u*v*SAB(end,end,2)+...
-                       u*(v-1)*SAB(end,1,2)+...
-                       v*(u-1)*SAB(1,end,2);
-            
-            SAB(n,p,3) = (1-u)*SAB(1,p,3)+...
-                         (1-v)*SAB(n,1,3)+...
-                             u*SAB(end,p,3) +...
-                             v*SAB(n,end,3) +...
-                   (u-1)*(1-v)*SAB(1,1,3)-...
-                           u*v*SAB(end,end,3)+...
-                       u*(v-1)*SAB(end,1,3)+...
-                       v*(u-1)*SAB(1,end,3);
+            if (m+n) <= nb_samples % in (P0,P1,P2) triangle conditions ; indices # nb segments
+                
+                % Barycentric coordinates.
+                l1 = m/nb_samples;
+                l2 = n/nb_samples;
+                l3 = (nb_samples - n - m)/nb_samples;
+                
+                % Transform the barycentric coordinates.
+                b1 = l1^warp;
+                b2 = l2^warp;
+                b3 = l3^warp;
+                
+                % Assure that they still sum up to 1.
+                db = (b1 + b2 + b3) - 1;
+                b1 = b1 - db*l1;
+                b2 = b2 - db*l2;
+                % b3 = b3 - db*l3;
+                
+                % translation vector
+                tv = b1*u + b2*v;
+                I(k,:) = (P1 + tv)';
+                k = k+1;
+                
+            end
             
         end
         
     end
     
-    SAB_x = SAB(:,:,1);
-    SAB_y = SAB(:,:,2);
-    SAB_z = SAB(:,:,3);
+else % if random_sampling
     
-    SAB_vector = [SAB_x(:)';SAB_y(:)';SAB_z(:)'];
+    nb_points = sum(1:nb_samples);
+    I = zeros(nb_points,Ndim);    
+    rand_coeff_vect = rand(2,nb_points-3);
+    f = sum(rand_coeff_vect,1) > 1;
+    rand_coeff_vect(:,f) = 1 - rand_coeff_vect(:,f);
+    
+    % Translation vectors
+    TV = repmat(rand_coeff_vect(1,:),[Ndim 1]).*repmat(u,[1 size(rand_coeff_vect,2)]) + ...
+         repmat(rand_coeff_vect(2,:),[Ndim 1]).*repmat(v,[1 size(rand_coeff_vect,2)]);
+    
+    for k = 1:size(TV,2)
+        
+        % translation vector
+        tv = TV(:,k);
+        I(k,:) = (P1 + tv)';
+        
+    end
+    
+    % Add the triangle vertices
+    I(end-2,:) = P1';
+    I(end-1,:) = P2';
+    I(end,:)   = P3';
+    I = unique(I','rows')';
     
 end
 
-SBC = Rmz * SAB_vector;
-SCA = Rmz * SBC;
-ABC = Rm  * SCA; 
 
-P = [SAB_vector';SBC';SCA';ABC'];
-P = unique(P,'rows');
+end % sample_and_curve_triangle
 
 
-end % Reuleaux_tetrahedron
+%% inflate_triangle_sample_from_opposite_vertex subfunction
+function P = inflate_triangle_sample_from_opposite_vertex(U, X, Rho)
+%
+% Authors : nicolas.douillet (at) free.fr, 2017-2024.
+%           Gerd Wachsmuth,                     2021.
+
+
+% We are looking for t such that
+%   || t U - X ||^2 = Rho^2
+% This is a quadratic equation.
+
+% Discriminant
+D = sum(U.*X, 2).^2 - sum(U.^2,2).*(sum(X.^2,2) - Rho^2);
+
+% We take the positive solution
+t = (sum(U.*X, 2) + sqrt(D)) ./ sum(U.^2,2);
+
+P = t.*U;
+
+
+end % inflate_triangle_sample_from_opposite_vertex
